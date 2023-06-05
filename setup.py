44a3880bda5e15cc7a236fc1a35d5a82384d9451
kyaryunha <kyaryunha@naver.com>
2023-06-05 11:17:02 +0900

    update readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="midi-clip",
-    version="0.8",
+    version="0.9",
     packages=find_packages(),
     description="A python package for midi clip.",
     long_description=long_description,
