4abf1ee2ca34ad5602c77999780c67d387ade00f
kyaryunha <kyaryunha@naver.com>
2023-06-05 11:30:36 +0900

    add keywords at setuppy

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="midi-clip",
-    version="0.9",
+    version="0.10",
     packages=find_packages(),
     description="A python package for midi clip.",
     long_description=long_description,
@@ -16,6 +16,7 @@ setup(
     license="ISC",
     license_file="LICENSE.md",
     license_file_content_type="text/markdown",
+    keywords="midi MIDI mir music audio sound midi-clip python python3",
     url="https://github.com/kyaryunha/midi-clip",
     install_requires=[
         "mido",
